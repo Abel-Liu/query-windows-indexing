3cd6b2f02ad622250c5ac9a5bb592651dd739416
Abel-Liu <abel.liu.china@outlook.com>
2015-05-07 20:28:53 +0800

    init

---
+++ Query Indexing/Query Indexing/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Query_Indexing._Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Query Indexing</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Contents:<asp:TextBox runat="server" ID="txtContent"></asp:TextBox>
+            <asp:Button runat="server" ID="btnSearch" Text="Go" OnClick="btnSearch_Click" />
+            <br />
+            <br />
+            <asp:TextBox TextMode="MultiLine" runat="server" ID="txtResult" ReadOnly="true" Rows="8" Width="500"></asp:TextBox>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Query Indexing/Query Indexing/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.OleDb;
+
+namespace Query_Indexing
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            string connectionString = "Provider=Search.CollatorDSO;Extended Properties=\"Application=Windows\"";
+            OleDbConnection connection = new OleDbConnection(connectionString);
+
+            string query = @"SELECT System.ItemPathDisplay FROM SystemIndex WHERE FREETEXT('" + this.txtContent.Text + "')";
+            OleDbCommand cmd = new OleDbCommand(query, connection);
+            connection.Open();
+
+            List<string> result = new List<string>();
+
+            OleDbDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                result.Add(reader.GetString(0));
+            }
+
+            connection.Close();
+
+            this.txtResult.Text = string.Join("\r\n", result.ToArray());
+        }
+    }
+}
